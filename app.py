5379198707b9fbd38a5bbb27159394c46c3eb7ec
AmoliShah <amolirshah1@gmail.com>
2017-04-10 17:25:12 +0530

    highlight

--- app.py
+++ app.py
@@ -10,58 +10,80 @@ import os
 import tkFileDialog
 import tkFont
 import sys
+import operator, itertools
 
 class PaintApp(Frame):
     def __init__(self,parent,posx,posy,*kwargs):
-        Frame.__init__(self, parent)   
-        self.parent = parent    
+        Frame.__init__(self, parent)
+        self.parent = parent
         self.initUI()
         self.eraserMode = False
         self.parent = parent
         self.posx = posx
         self.posy = posy
-        self.sizex = 2000
-        self.sizey = 300
+        self.sizex = 1500
+        self.sizey = 700
         self.color = 'black'
+        self.color1 = 'yellow'
         self.b1 = "up"
         self.xold = None
-        self.yold = None 
+        self.yold = None
         self.line_width = 10
         self.save = False
         self.eraseColor = 'white'
+        self.flag = False
+        self.image=Image.new("RGBA",(self.sizex,self.sizey),(255,255,255))
         
+        self.draw=ImageDraw.Draw(self.image)
         self.drawing_area = tk.Canvas(self.parent,width=self.sizex,height=self.sizey,bg = 'white')
-        for a in range(0,2000,35):
-            self.drawing_area.create_line(0 ,0 +a,2000,0 + a,fill = 'black')
-        self.drawing_area.create_line(100 ,0,100,300,fill = 'red')
-        
+        self.drawPage()
+
         self.drawing_area.place(x=self.posx,y=self.posy)
-        self.drawing_area.bind("<Motion>", self.motion)
-        self.drawing_area.bind("<ButtonPress-1>", self.b1down)
-        self.drawing_area.bind("<ButtonRelease-1>", self.b1up)
-        self.drawing_area.config(scrollregion=self.drawing_area.bbox("all"))
+
+        #self.drawing_area.config(scrollregion=self.drawing_area.bbox("all"))
+
 
         #pen button
-        self.pen =  tk.Button(self.parent,text="pen",width=10,bg='green',command=self.use_pen,cursor='pencil')
+        self.pen =  tk.Button(self.parent,text="Pen",width=10,bg='green',command=self.use_pen,cursor='pencil')
         self.pen.grid(row=0, column=0)
-        
+
         #eraser button
-        self.clearPoint=tk.Button(self.parent,text="eraser ",width=10,bg='blue',command=self.del_,cursor='icon')
+        self.clearPoint=tk.Button(self.parent,text="Eraser ",width=10,bg='blue',command=self.del_,cursor='icon')
         self.clearPoint.grid(row=1,column=0)
 
-        self.image=Image.new("RGB",(400,400),(255,255,255))
-        self.draw=ImageDraw.Draw(self.image)
+        #Highlight button
+        self.highlight=tk.Button(self.parent,text="Highlight ",width=10,bg='red',command=self.highLight,cursor='pencil')
+        self.highlight.grid(row=2,column=0)
 
         self.file_opt = options = {}
         options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
         options['initialfile'] = 'myfile.txt'
         options['parent'] = parent
 
+    def highLight(self):
+        self.flag = True
+        if self.flag == True:
+            self.drawing_area.bind("<Motion>", self.highLight1)
+            self.drawing_area.bind("<ButtonPress-1>", self.b1down)
+            self.drawing_area.bind("<ButtonRelease-1>", self.b1up)
+        
+        self.flag = False
+
+
+        
+    def drawPage(self):
+        for a in range(0,3000,35):
+            self.drawing_area.create_line(0 ,0 +a,2000,0 + a,fill = 'black')
+            self.draw.line([(0,0+a),(2000,0+a)],fill='black')
+        self.drawing_area.create_line(100 ,0,100,700,fill = 'red')
+        self.draw.line([(100,0),(100,700)],fill='red')
+
+
     def initUI(self):
         self.parent.title("KINTU")
         menubar = Menu(self.parent)
         self.parent.config(menu=menubar)
-        
+
         fileMenu = Menu(menubar)
         fileMenu.add_command(label="New ", command=self.new)
         fileMenu.add_command(label="Open", command=self.add)
@@ -69,63 +91,81 @@ class PaintApp(Frame):
         fileMenu.add_command(label="Clear All", command=self.clear)
         fileMenu.add_command(label="Exit", command=self.exit)
         menubar.add_cascade(label="File", menu=fileMenu)
+
         editMenu = Menu(menubar)
-        
         editMenu.add_command(label="Insert Image", command=self.openImage)
         menubar.add_cascade(label="Edit", menu=editMenu)
+
         toolsMenu = Menu(menubar)
         toolsMenu.add_command(label="color", command=self.choose_color)
-    
-    def new(self):
+        menubar.add_cascade(label="Tool", menu=toolsMenu)
 
+    def new(self):
         self.exit()
         root=tk.Tk()
-        root.wm_geometry("%dx%d+%d+%d" % (300, 300, 10, 10))
+        root.wm_geometry("%dx%d+%d+%d" % (self.sizex, self.sizey, 10, 10))
         root.config(bg='white')
         PaintApp(root,10,10)
         root.mainloop()
 
     def exit(self):
-
         if (self.save==False):
             self.asksaveasfilename()
-        
+
         root.destroy()
 
+
     def add(self):
-        print "exit"    
-    
+
+        fname = tkFileDialog.askopenfilename(filetypes = (("Template files", "*.jpg"), ("All files", "*")))
+        img = ImageTk.PhotoImage(Image.open(fname))
+        im = Image.open(fname).convert("RGBA")
+        x,y = im.size;
+        self.clear()
+        self.drawPage();
+        self.drawing_area.create_image(int(x/2),int(y/2),image=img)
+        self.image.paste(im,im)
+        imglabel = Label(self.parent,image=img).grid(row=4,column=10)
+
     def asksaveasfilename(self):
         filename = tkFileDialog.asksaveasfilename(**self.file_opt)
 
         if filename:
-           
+
            self.image.save(filename)
            self.save = True
-            
-    def use_pen(self):
 
-         self.eraserMode = False
-         self.color = 'black'
+    def use_pen(self):
+        self.eraserMode = False
+        self.color = 'black'
+        if self.flag == False:
+            self.drawing_area.bind("<Motion>", self.motion)
+            self.drawing_area.bind("<ButtonPress-1>", self.b1down)
+            self.drawing_area.bind("<ButtonRelease-1>", self.b1up)
+
+
 
     def openImage(self):
-        
+
         fname = tkFileDialog.askopenfilename(filetypes = (("Template files", "*.png"), ("All files", "*")))
         img = ImageTk.PhotoImage(Image.open(fname))
+        im = Image.open(fname).convert("RGBA")
+        x,y = im.size;
         #img = Image.resize((250, 250), Image.ANTIALIAS)
-        imagesprite = self.drawing_area.create_image(20,20,image=img)
-        tk.Frame(self.parent) 
+        a1 = self.drawing_area.create_image(int(x/2),int(y/2),image=img)
+
+        self.image.paste(im,im)
+
         #Displaying ImageTk
-        imglabel =  Label(self.parent,image=img).grid(row=2,column=1)
+        imglabel = Label(self.parent,image=img).grid(row=4,column=10)
+
 
     def clear(self):
 
         self.drawing_area.delete("all")
-        self.image=Image.new("RGB",(300,300),(255,255,255))
+        self.image=Image.new("RGBA",(self.sizex,self.sizey),(255,255,255))
         self.draw=ImageDraw.Draw(self.image)
-        for a in range(0,2000,35):
-            self.drawing_area.create_line(0 ,0 +a,2000,0 + a,fill = 'black')
-            self.drawing_area.create_line(100 ,0,100,300,fill = 'red')
+        self.drawPage()
 
     def b1down(self,event):
         self.b1 = "down"
@@ -136,31 +176,50 @@ class PaintApp(Frame):
         self.yold = None
 
     def motion(self,event):
-        
+        #print("Motion")
         if self.b1 == "down":
             if self.xold is not None and self.yold is not None:
-                
+
                 if(self.eraserMode == True):
-                    
+
                     self.parent.config(cursor = 'icon')
-                    event.widget.create_line(self.xold,self.yold,event.x,event.y,smooth='true',width=3,fill='white')     		                  
+                    event.widget.create_line(self.xold,self.yold,event.x,event.y,smooth='true',width=3,fill='white')
                     self.draw.line(((self.xold,self.yold),(event.x,event.y)),(255,255,255),width=3)
-                    for a in range(0,2000,35):
-                        self.drawing_area.create_line(0 ,0 +a,2000,0 + a,fill = 'black')
-                        self.drawing_area.create_line(100 ,0,100,300,fill = 'red')
-                    
-       			     
-                if(self.eraserMode == False):      
+                    self.drawPage()
+
+
+                if(self.eraserMode == False):
 
                     self.parent.config(cursor = 'pencil')
+                    #print(self.color)
                     event.widget.create_line(self.xold,self.yold,event.x,event.y,smooth='true',width=3,fill=self.color)
-                    self.draw.line(((self.xold,self.yold),(event.x,event.y)),(self.color),width=3)     
+                    self.draw.line(((self.xold,self.yold),(event.x,event.y)),(self.color),width=3)
+                    #event.widget.create_line(self.xold,self.yold,event.x,event.y,smooth='true',width=3,fill=self.color)
+
         self.xold = event.x
         self.yold = event.y
-                    
+        
+
+    def highLight1(self,event):
+    
+        if self.b1 == "down":
+            if self.xold is not None and self.yold is not None:
+                if(self.eraserMode == False):
+
+                    self.parent.config(cursor = 'pencil')
+                    #print(self.color)
+                    event.widget.create_line(self.xold,self.yold,event.x,event.y,smooth='true',width=10,fill=self.color1,stipple='gray50')
+                    self.draw.line(((self.xold,self.yold),(event.x,event.y)),(self.color1),width=3)
+                    #event.widget.create_line(self.xold,self.yold,event.x,event.y,smooth='true',width=3,fill=self.color)
+
+        self.xold = event.x
+        self.yold = event.y
+    
+    
+
     def del_(self):
         self.eraserMode = True
-                
+
     def choose_color(self):
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
